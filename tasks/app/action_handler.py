9d83c6ef0f908c995857cd23db31ce69d5ebd5ec
Pandu Shadahi Joseph <2300032260@kluniversity.in>
2026-04-12 21:48:44 +0530

    remove tasks folder again - final architecture

--- tasks/app/action_handler.py
+++
@@ -1,6 +0,0 @@
-class ActionHandler:
-    VALID_ACTIONS = {"administer_drug", "adjust_ventilator", "request_lab", "escalate_care"}
-
-    @staticmethod
-    def is_valid(action: str) -> bool:
-        return action in ActionHandler.VALID_ACTIONS
