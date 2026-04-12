f8a4387195551f16da05836c7c9e52447732b0f9
Pandu Shadahi Joseph <2300032260@kluniversity.in>
2026-04-13 00:13:12 +0530

    Add task configurations to __init__.py
    Initialized task configurations for easy, medium, and hard tasks.

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,1 +1,9 @@
-# Empty file so python resolves the folder as a library package
+from .easy import TASK_CONFIG as EASY_TASK
+from .medium import TASK_CONFIG as MEDIUM_TASK
+from .hard import TASK_CONFIG as HARD_TASK
+
+TASKS = {
+    "easy": EASY_TASK,
+    "medium": MEDIUM_TASK,
+    "hard": HARD_TASK
+}
